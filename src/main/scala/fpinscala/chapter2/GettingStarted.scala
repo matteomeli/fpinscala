78289a74a5b5f7b4f68a9670d877c3e25a70b6ee
Matteo Meli <matteo.meli@gmail.com>
2016-11-18 18:40:54 +0000

    Improve solutio to exercise 2.1 with tailrec inner function

--- src/main/scala/fpinscala/chapter2/GettingStarted.scala
+++ src/main/scala/fpinscala/chapter2/GettingStarted.scala
@@ -5,9 +5,13 @@ object GettingStarted {
     println("Functional Programming in Scala!")
   }
 
+  // Exercise 2.1
   def fibonacci(n: Int): Int = {
-    if (n == 0) 0
-    else if (n == 1) 1
-    else fibonacci(n - 1) + fibonacci(n - 2)
+    @annotation.tailrec
+    def loop(n: Int, prev: Int, curr: Int): Int = {
+      if (n == 0) prev
+      else loop(n - 1, curr, prev + curr)
+    }
+    loop(n, 0, 1)
   }
 }

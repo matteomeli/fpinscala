bfcee382ee99a2b8b262c61414d71c8fcce1108b
Matteo Meli <matteo.meli@gmail.com>
2016-11-18 18:05:59 +0000

    Fix deprecated procedure syntax

--- src/main/scala/fpinscala/chapter2/GettingStarted.scala
+++ src/main/scala/fpinscala/chapter2/GettingStarted.scala
@@ -1,7 +1,7 @@
 package fpinscala.chapter2
 
 object GettingStarted {
-  def main(args: Array[String]) {
+  def main(args: Array[String]): Unit = {
     println("Functional Programming in Scala!")
   }
 }
